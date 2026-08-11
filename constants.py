198510d806a1848807357c25a029f195feac6d47
Siehste <siehste@pm.me>
2026-08-11 19:25:03 +0200

    Added player and basic movement

--- constants.py
+++ constants.py
@@ -1,2 +1,6 @@
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
+SCREEN_WIDTH = 1280 #Screensize
+SCREEN_HEIGHT = 720 #Screensize
+PLAYER_RADIUS = 20 #Radius of player's ship
+LINE_WIDTH = 2 #Thickness of drawn line of player ship
+PLAYER_TURN_SPEED = 300 #Rotation speed of ship
+PLAYER_SPEED = 200 #Speed of player ship
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 from logger import log_state
 
 def main():
@@ -11,12 +12,33 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player_one = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #size of player
+
+
+
+
+    #Continuous GameLoop
     while True:
         log_state()
+
+        #Quit Game/Window with X
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        #Black background
         screen.fill("black")
+
+        #Draw player on screen
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         #print(f"{dt}")
